e9c5235bad04b6f13fa88489c608193ac6ea4c0c
Melqui Martins <115431086+melquimartins@users.noreply.github.com>
2026-07-30 15:32:40 -0300

    chore: commit inicial do projeto

---
+++ src/test/java/io/github/melquimartins/smartstock/SmartStockApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.melquimartins.smartstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartStockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
